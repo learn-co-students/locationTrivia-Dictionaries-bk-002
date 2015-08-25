55a5904f2574f6436fbaa9c143ee3ffca0f9ef72
azmuth94 <dannyaksenov@gmail.com>
2015-08-25 11:42:11 -0400

    swift is definitely better than this

--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -16,10 +16,49 @@
     return YES;
 }
 
-/** 
- 
- *  Write your method implementations here.
- 
- */
+- (NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location toLength:(NSUInteger)length {
+    if ([location[@"name"]length] >= length) {
+    NSMutableString *name = [[location valueForKey:@"name"] mutableCopy];
+    NSString *truncatedName = [name substringToIndex:length];
+    return truncatedName;
+    } else {
+        return location[@"name"];
+    }
+}
+
+- (NSDictionary *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude  longitude:(CGFloat)longitude {
+    NSMutableDictionary *dictionaryForLocation = [NSMutableDictionary new];
+    [dictionaryForLocation setObject:name forKey:@"name"];
+    [dictionaryForLocation setObject:@(latitude) forKey:@"latitude"];
+    [dictionaryForLocation setObject:@(longitude) forKey:@"longitude"];
+    return dictionaryForLocation;
+}
+
+- (NSArray *)namesOfLocations:(NSArray *)locations {
+    NSMutableArray *namesOfLocations = [NSMutableArray new];
+    for (NSDictionary *location in locations) {
+        [namesOfLocations addObject:location[@"name"]];
+    }
+    return namesOfLocations;
+}
+
+- (BOOL)dictionaryIsValidLocation:(NSDictionary *)dictionary {
+    NSArray *locationKeys = [dictionary allKeys];
+    
+    if ([locationKeys count] == 3 && [locationKeys containsObject:@"name"] && [locationKeys containsObject:@"latitude"] && [locationKeys containsObject:@"longitude"]) {
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
+- (NSDictionary *)locationNamed:(NSString *)name inLocations:(NSArray *)locations {
+    for (NSDictionary *location in locations) {
+        if ([location[@"name"] isEqualToString:name]) {
+            return location;
+        }
+    }
+    return nil;
+}
 
 @end
